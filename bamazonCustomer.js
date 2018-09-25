1d70d31ddaf2669f1f7fd732dbce6fbba17b3e37
benjamin j stone  jr <RadioAktivemusic@benjamins-imac.attlocal.net>
2018-09-25 11:43:59 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ require('console.table');
 
 const connection = myql.creatConnection({
     host:"localhost",
-    port:3306,
+    port:8889,
     user:"root",
     password:"",
     database:"bamazon"
@@ -18,28 +18,29 @@ connection.connect(err=>{
     loadProducts();
 }); 
 
- const loadProducts() => {
+ const loadProducts =  () => {
     connection.query("SELECT * FROM products", (err,res) => {
-        if (err)
+      if (err) {
             throw err;
     }
     console.table(res);
     promptCustomerForItem(res);
 });
+    const promptCustomerForItem = inventory => {
 
-}
+ 
+  
 inquirer
     .prompt([
       {
         type: "input",
         name: "choice",
         message: "What is the ID of the item you would you like to purchase? [Quit with Q]",
-        validate: function(val) {
-          return !isNaN(val) || val.toLowerCase() === "q";
+        validate:val =>  !isNaN(val) || val.toLowerCase() === "q"
         }
-      }
+      
     ])
-    .then(function(val) {
+    .then(val =>{
       // Check if the user wants to quit the program
       checkIfShouldExit(val.choice);
       const choiceId = parseInt(val.choice);
@@ -48,27 +49,37 @@ inquirer
       console.log(product)
 
       // If there is a product with the id the user chose, prompt the customer for a desired quantity
-      if (product) {
+        if (product) {
         // Pass the chosen product to promptCustomerForQuantity
         promptCustomerForQuantity(product);
-      }
+        }
       else {
         // Otherwise let them know the item is not in the inventory, re-run loadProducts
         console.log("\nThat item is not in the inventory.");
         loadProducts();
-      }
+       }
     });
+  } 
 
-    const checkInventory = choiceId, inventory)=> {
-        for(var i =0;i < inventory.length;)
-    }
-    const checkIfShouldExit = choice => {
+     const checkInventory = (choiceId, inventory) => {
+        const item = inventory.filter(item => item.item_id === choiceId);
+        return item.length > 0 ? item[0] :null;
+  }
+
+      const makePurchase = (product, quantity) => {
+
+      }
+      const promptCustomerForQuantity = product => {
+
+      }
+    
+        const checkIfShouldExit = choice => {
         if (choice.toLowerCase()=== "q"){
-            console.log("don't let scrren hit you.");
+            console.log("don't let screen hit you.");
             process.exit(0);
         }
-    });
-}
+    } 
+
 
 inquirer
 .prompt([
@@ -76,15 +87,13 @@ inquirer
     type: "input",
     name: "quantity",
     message: "How many would you like? [Quit with Q]",
-    validate: function(val) {
-      return val > 0 || val.toLowerCase() === "q";
+    validate: val => (val) || val.toLowerCase() === "q"
     }
-  }
-])
-.then(function(val) {
+  ])
+.then((val => {
   // Check if the user wants to quit the program
   checkIfShouldExit(val.quantity);
-  var quantity = parseInt(val.quantity);
+  const quantity = parseInt(val.quantity);
 
   // If there isn't enough of the chosen product and quantity, let the user know and re-run loadProducts
   if (quantity > product.stock_quantity) {
@@ -96,6 +105,8 @@ inquirer
     makePurchase(product, quantity);
   }
 });
+ 
+}
 
 connection.query(
     "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
@@ -110,5 +121,5 @@ connection.query(
   const promptCustomerForQuantity = product =>{
 
   }
-
+ 
   
